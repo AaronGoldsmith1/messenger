b2055083124c25a49bf576a0f7bd5ea08240d51f
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-06-23 11:53:11 -0700

    reorder require statements app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
-const cookieParser = require("cookie-parser");
 const createError = require("http-errors");
 const express = require("express");
 const { join } = require("path");
 const logger = require("morgan");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const db = require("./db");
