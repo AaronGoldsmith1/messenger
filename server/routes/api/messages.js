8254afe816bfbbe29badc91efb81bbdbb093b55e
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-06-23 18:11:17 -0700

    ensure conversation exists between sender and recipient and matches request

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -11,16 +11,19 @@ router.post("/", async (req, res, next) => {
     const senderId = req.user.id;
     const { recipientId, text, conversationId, sender } = req.body;
 
-    // if we already know conversation id, we can save time and just add it to message and return
-    if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
-      return res.json({ message, sender });
-    }
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
     let conversation = await Conversation.findConversation(
       senderId,
       recipientId
     );
+    
+    //ensure conversation exists between sender and recipient and matches request
+    if (conversation && conversationId === conversation.id) {
+      const message = await Message.create({ senderId, text, conversationId });
+      return res.json({ message, sender });
+    } else if (conversation && conversationId != conversation.id) {
+      return res.sendStatus(403);
+    }
 
     if (!conversation) {
       // create conversation
