2c61f8220239a40d4c88e945490d7bfad1dfa073
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-06-23 19:10:38 -0700

    update conversation model

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -22,4 +22,88 @@ Conversation.findConversation = async function (user1Id, user2Id) {
   return conversation;
 };
 
+// With two user Ids, find a conversation and all associated messages 
+Conversation.findOneConversationAndMessages = async function (senderId, recieverId) {
+  const conversation = await Conversation.findOne({
+    where: {
+      user1Id: {
+        [Op.or]: [senderId, recieverId]
+      },
+      user2Id: {
+        [Op.or]: [senderId, recieverId]
+      }
+    },
+    attributes: ["id"],
+    order: [[Message, "createdAt", "ASC"]],
+    include: [
+      { model: Message },
+      {
+        model: User,
+        as: "user1",
+        where: {
+          id: {
+            [Op.not]: senderId,
+          },
+        },
+        attributes: ["id", "username", "photoUrl"],
+        required: false,
+      },
+      {
+        model: User,
+        as: "user2",
+        where: {
+          id: {
+            [Op.not]: senderId,
+          },
+        },
+        attributes: ["id", "username", "photoUrl"],
+        required: false,
+      },
+    ],
+  });
+
+  return conversation
+}
+
+// For a given user, find all conversations and associated messages
+Conversation.findAllConversationsAndMessages = async function (userId) {
+  const conversations = await Conversation.findAll({
+    where: {
+      [Op.or]: {
+        user1Id: userId,
+        user2Id: userId,
+      },
+    },
+    attributes: ["id"],
+    order: [[Message, "createdAt", "ASC"]],
+    include: [
+      { model: Message },
+      {
+        model: User,
+        as: "user1",
+        where: {
+          id: {
+            [Op.not]: userId,
+          },
+        },
+        attributes: ["id", "username", "photoUrl"],
+        required: false,
+      },
+      {
+        model: User,
+        as: "user2",
+        where: {
+          id: {
+            [Op.not]: userId,
+          },
+        },
+        attributes: ["id", "username", "photoUrl"],
+        required: false,
+      },
+    ],
+  })
+
+  return conversations
+}
+
 module.exports = Conversation;
