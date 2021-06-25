30a9bfbf0b295c58adf5bc068d6b9f7339eab138
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-06-25 15:06:01 -0700

    add avatar under last read message

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,20 +1,39 @@
-import React from "react";
-import { Box } from "@material-ui/core";
+import React, {useState} from "react";
+import { Box, Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
-const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+const useStyles = makeStyles(() => ({
+  avatar: {
+    width: 20,
+    height: 20,
+    float: 'right',
+    marginTop: 5,
+    },
+}));
 
+
+const Messages = (props) => {
+  const classes = useStyles();
+  const { messages, otherUser, userId } = props;
+  const lastReadMessage = messages.filter((message) => message.senderId === userId && !message.unread).pop();
+  
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.map((message, idx) => {
         const time = moment(message.createdAt).format("h:mm");
-
         return message.senderId === userId ? (
+          <>
           <SenderBubble key={message.id} text={message.text} time={time} />
+          { message.id === lastReadMessage?.id ? 
+            <Avatar alt={otherUser.username} src={otherUser.photoUrl} className={classes.avatar}></Avatar> : ''}
+          </>
         ) : (
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+
+
+
         );
       })}
     </Box>
