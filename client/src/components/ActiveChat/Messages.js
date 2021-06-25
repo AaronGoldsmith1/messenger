73c0c19276c841fe492620d42818c4c16d6e1ce4
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-06-25 16:29:46 -0700

    remove unused idx

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -9,7 +9,7 @@ const Messages = (props) => {
   
   return (
     <Box>
-      {messages.map((message, idx) => {
+      {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
         return message.senderId === userId ? (
           <SenderBubble 
